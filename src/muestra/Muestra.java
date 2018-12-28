4ed6248caf90d82fdeaeb98bf215d94931d7a024
Ger1211 <gmcabrera121190@gmail.com>
2018-12-28 00:54:09 -0300

    Refactor de nombre de mensajes a nombres mas representativos

--- src/muestra/Muestra.java
+++ src/muestra/Muestra.java
@@ -10,15 +10,7 @@ import ubicacion.ZonaDeCobertura;
 import usuario.Usuario;
 
 public class Muestra {
-
-	/*	a = Vinchuca;
-	 *  b = Chinche Foliada
-	 *  c = Phtia-Chinche
-	 *  d = Ninguna
-	 *  e = Imagen poco clara
-	 *  
-	 */
-
+	
 	private String tipoMuestra;
 	private Ubicacion ubicacion;
 	private Usuario usuario;
@@ -27,22 +19,20 @@ public class Muestra {
 	private List<ZonaDeCobertura> misZonasDeCobertura;
 	
 	public Muestra(String tipoMuestra, Ubicacion ubicacion, Usuario usuario, Verificacion verificacion) {
-		// TODO Auto-generated constructor stub
 		this.tipoMuestra = tipoMuestra;
 		this.ubicacion = ubicacion;
 		this.usuario = usuario;
 		this.misZonasDeCobertura = new ArrayList<>();
-		// Creacion y agregado de la verificacion base:
-		this.setearNivelDeVerificacion(verificacion);
+		
+		this.setearELNivelDeLaVerificacionInicial(verificacion);
 		this.verificaciones = new ArrayList<>();
 		this.verificaciones.add(verificacion);
 		
 		
 	}
 
-	private void setearNivelDeVerificacion(Verificacion verificacion) {
-		// TODO Auto-generated method stub
-		if(verificacion.getUsuarioVerificacion().getTipoDeUsuario().esUsuarioBasico()) {
+	private void setearELNivelDeLaVerificacionInicial(Verificacion verificacion) {
+		if(esVerificacionHechaPorUnUsuarioBasico(verificacion)) {
 			this.nivelDeVerificacion =  new NivelDeVerificacionBajo();
 		}
 		else {
@@ -51,18 +41,19 @@ public class Muestra {
 		
 	}
 
+	private Boolean esVerificacionHechaPorUnUsuarioBasico(Verificacion verificacion) {
+		return verificacion.getUsuarioVerificacion().getTipoDeUsuario().esUsuarioBasico();
+	}
+
 	public String getTipoMuestra() {
-		// TODO Auto-generated method stub
 		return this.tipoMuestra;
 	}
 
 	public Ubicacion getUbicacion() {
-		// TODO Auto-generated method stub
 		return this.ubicacion;
 	}
 
 	public Usuario getUsuarioMuestra() {
-		// TODO Auto-generated method stub
 		return this.usuario;
 	}
 
@@ -71,37 +62,45 @@ public class Muestra {
 	}
 	
 	public Boolean contiene(Verificacion verificacion) {
-		// TODO Auto-generated method stub
 		return this.verificaciones.contains(verificacion);
 	}
 
 	public void agregarVerificacion(Verificacion verificacion) {
-		// TODO Auto-generated method stub
-		if(verificaciones.size()<3) {
+		if(esMaximaCantidadDeVerificaciones()) {
 		this.notificarAMisZonas(); 
 		this.nivelDeVerificacion.actualizarNivelDeVerificacion(verificacion,this);
 		this.verificaciones.add(verificacion);
 		}
 	}
+
+	private boolean esMaximaCantidadDeVerificaciones() {
+		return verificaciones.size()<3;
+	}
 	
 	public Boolean usuarioNoVerifico(Usuario usuario) {
-		// TODO Auto-generated method stub
 		Boolean resultado = true;
 		for(Verificacion verificacion: verificaciones) {
-			resultado = resultado && !verificacion.getUsuarioVerificacion().equals(usuario);
+			resultado = resultado && usuarioNoEsElVerificadorInicialDeLaMuestra(usuario, verificacion);
 		}
 		return resultado;
 	}
 
+	private boolean usuarioNoEsElVerificadorInicialDeLaMuestra(Usuario usuario, Verificacion verificacion) {
+		return !verificacion.getUsuarioVerificacion().equals(usuario);
+	}
+
 	public List<Muestra> muestrasAMenosDe(Double kilometros, List<Muestra> muestras) {
-		// TODO Auto-generated method stub
 		ArrayList<Muestra> resultado = new ArrayList<>();
 		for(Muestra muestra : muestras) {
-			if(this.getUbicacion().ubicacionEstaAMenosDe(kilometros, muestra.getUbicacion())) {
+			if(miUbicacionActualEstaAMenosKilometrosDeLaUbicacionDeLaMuestra(kilometros, muestra)) {
 				resultado.add(muestra);
 			}
 		}
 		return resultado;
+	}
+
+	private Boolean miUbicacionActualEstaAMenosKilometrosDeLaUbicacionDeLaMuestra(Double kilometros, Muestra muestra) {
+		return this.getUbicacion().ubicacionEstaAMenosDe(kilometros, muestra.getUbicacion());
 	}
 
 
@@ -126,17 +125,14 @@ public class Muestra {
 	}
 
 	public NivelDeVerificacion getNivelDeVerificacion() {
-		// TODO Auto-generated method stub
 		return this.nivelDeVerificacion;
 	}
 
 	public void setNivelDeVerificacion(NivelDeVerificacion nivelDeVerificacion) {
-		// TODO Auto-generated method stub
 		this.nivelDeVerificacion = nivelDeVerificacion;
 	}
 
 	public String verificar() {
-		// TODO Auto-generated method stub
 		// Verificar Usuarios Especialistas
 		List<Verificacion> resultadoEspecialistas = this.verificaciones.stream().filter(verificacion -> verificacion.verificadorEsEspecialista()).collect(Collectors.toList());
 		for(Verificacion verificacion1 : resultadoEspecialistas) {
@@ -162,14 +158,12 @@ public class Muestra {
 }
 
 	private Boolean mayoriaVotos(Verificacion verificacion, List<Verificacion> verificaciones) {
-		// TODO Auto-generated method stub
 		List<Verificacion> listaAFavor = verificaciones.stream().filter(verificacionMuestra -> verificacionMuestra.votoAFavor(verificacion)).collect(Collectors.toList());
 		List<Verificacion> listaEnContra = verificaciones.stream().filter(verificacionMuestra -> verificacionMuestra.votoEnContra(verificacion)).collect(Collectors.toList());	
 		return listaAFavor.size() > listaEnContra.size();
 	}
 
 	public void agregarMisZonasDeCobertura(List<ZonaDeCobertura> zonasDeCobertura) {
-		// TODO Auto-generated method stub
 		this.misZonasDeCobertura = zonasDeCobertura.stream().filter(zona -> zona.perteneceAZonaDeCobertura(this.ubicacion)).collect(Collectors.toList());
 	}
 	
@@ -180,12 +174,10 @@ public class Muestra {
 	}
 
 	public void notificarAMisZonas() {
-		// TODO Auto-generated method stub
 		this.misZonasDeCobertura.stream().forEach(zona-> zona.notificar(this));
 	}
 
 	public List<ZonaDeCobertura> getZonasDeCobertura() {
-		// TODO Auto-generated method stub
 		return this.misZonasDeCobertura;
 	}
 
@@ -195,7 +187,6 @@ public class Muestra {
 	}
 
 	public LocalDate getFechaUltimaVerificacion() {
-		// TODO Auto-generated method stub
 		return this.verificaciones.get(this.verificaciones.size() -1).getFechaVerificacion();
 	}
 
